813f0fbda043f1343bc5636bfe8c9e1d02cdd2db
karendiaz23 <kareendiaz23@gmail.com>
2025-03-25 17:14:15 -0300

    Jueguito Mystery

---
+++ mistery.sql
@@ -0,0 +1,6 @@
+SELECT * FROM crime_scene_report
+WHERE date = 20180115 AND type = 'murder' AND city like 'sql city'
+
+20180115	murder	Security footage shows that there were 2 witnesses.
+The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".
+SQL City
