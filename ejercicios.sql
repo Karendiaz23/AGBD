c37c965ece26fa422290f53dbc41638a4eaac077
kareendiaz <kareendiaz23@gmail.com>
2025-04-15 16:42:15 -0300

    insert, update y delete

--- ejercicios.sql
+++ ejercicios.sql
@@ -120,5 +120,146 @@ WHERE f.title = "ALABAMA DEVIL"
 
 
 
+15/04/25
+
+
+
+/*ejercicio N13*/
+
+
+SELECT f.title AS nombre,f.length AS duracion,c.name AS categoria
+FROM film f
+INNER JOIN film_category fc ON f.film_id = fc.film_id
+INNER JOIN category c ON fc.category_id = c.category_id
+ORDER BY f.length DESC;
+
+
+
+/*ejercicio N14*/
+
+SELECT f.title,count (a.actor_id)AS actores 
+FROM film f
+INNER JOIN film_actor fa ON f.film_id = fa.film_id
+INNER JOIN actor a on fa.actor_id = a.actor_id
+WHERE f.title LIKE 'W%'
+GROUP BY f.film_id
+HAVING COUNT(fa.actor_id) >= 5;
+
+/*ejercicio N15*/
+SELECT SUM(p.amount), c.first_name
+FROM customer c
+INNER JOIN payment p on c.customer_id = p.customer_id
+group by p.customer_id
+
+
+/*EJERCICIO N16*/
+
+
+SELECT f.title, f.rental_duration, a.first_name, ca.name
+FROM category ca
+INNER JOIN film_category fc on ca.category_id = fc.category_id
+INNER JOIN film f ON f.film_id = fc.film_id
+INNER JOIN film_actor fa ON f.film_id = fa.film_id
+inner join actor a on a.actor_id = fa.actor_id
+ORDER BY rental_duration ASC;
+
+
+/*ejercicio N17*/
+
+
+SELECT c.last_name AS apellido,ci.city,co.country,a.address,r.rental_id,p.payment_id,p.amount
+FROM customer c
+INNER JOIN address a ON c.address_id = a.address_id
+INNER JOIN city ci ON a.city_id = ci.city_id
+INNER JOIN country co ON ci.country_id = co.country_id
+INNER JOIN  rental  R on  c.customer_id = R.customer_id
+INNER JOIN  payment p on R.rental_id = p.rental_id
+GROUP BY c.customer_id
+ORDER BY p.amount DESC;
+
+
+
+/*
+iNSERT INTO actor (actor_id, first_name, last_name, last_update)
+VALUES ("201", "KAREN", "DIAZ","2020-02-20 08:08:11")
+
+UPDATE actor
+SET first_name = "REBE", last_name = "DIAZ"
+WHERE last_name = "DIAZ" AND first_name = "KAREN"
+
+DELETE FROM actor
+WHERE actor_id = 201
+*/
+
+
+
+/*EJERCICIO N18*/
+
+iNSERT INTO actor (actor_id, first_name, last_name, last_update)
+VALUES ("201", "KAREN", "DIAZ","2020-02-20 08:08:11")
+
+
+/*EJERCICIO N19*/
+iNSERT INTO actor (actor_id, first_name, last_name, last_update)
+VALUES ("202", "LUANA", "LOPEZ","2020-02-20 08:08:11")
+
+
+iNSERT INTO actor (actor_id, first_name, last_name, last_update)
+VALUES ("203", "LUCHO", "ALCARAZ","2020-02-20 08:08:11")
+
+
+/*ejercicio N20*/
+
+UPDATE actor
+SET first_name = 'EMMA', last_name = 'WATSON'
+WHERE actor_id = 201;
+
+UPDATE actor
+SET first_name = 'MARTINA', last_name = 'STOESELL'
+WHERE actor_id = 202;
+
+
+UPDATE actor
+SET first_name = 'MESSI', last_name = 'LEONEL'
+WHERE actor_id = 203;
+
+
+/*EJERCICO N21*/
+
+
+DELETE FROM actor
+WHERE actor_id = 201
+
+DELETE FROM actor
+WHERE actor_id = 202
+
+DELETE FROM actor
+WHERE actor_id = 203
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
