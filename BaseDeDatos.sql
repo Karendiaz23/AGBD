987a38a9192cb268e387928ffc8a5ea304f25c13
karendiaz <kareendiaz23@gmail.com>
2025-05-20 16:10:57 -0300

    casi terminada la base de datos

--- BaseDeDatos.sql
+++ BaseDeDatos.sql
@@ -5,7 +5,7 @@
 /*COMO CREAR UNA BASE DE DATOS*/
 
 CREATE TABLE Clientes (
-ClientesID INT PRIMARY KEY,
+ClientesID INTEGER PRIMARY KEY AUTOINCREMENT,
 Nombre VARCHAR(50),
 Apellido VARCHAR(50),/*se usa VARCHAR si es texto*/
 Dni INT)  /* si es numero se usa INT*/
@@ -15,9 +15,54 @@ CREATE TABLE Pagos (
 PagosID INT PRIMARY KEY,
 Monto INT,
 Metodo_Pagos VARCHAR(50)
-)
+);
+
+
+CREATE TABLE pedidos (
+pedidosID INT PRIMARY KEY,
+fecha INT,
+hora INT,
+cantidad INT); 
+
+/*otro ejemplo*/
+CREATE TABLE galletitas (
+galletitasID INT PRIMARY KEY,
+categoria VARCHAR(50),
+relleno VARCHAR(50),
+precio INT
+);
+
+
 
 /*ASI SE CARGAN DATOS A UNA BASE*/
-INSERT INTO Clientes(Nombre,Apellido,Dni)
+INSERT INTO Clientes(ClientesID,Nombre,Apellido,Dni)
 VALUES
-("luana","lopez","48112558")
+("1","luana","lopez","48112558");
+
+
+INSERT INTO pedidos(pedidosID,fecha,hora ,cantidad)
+VALUES
+("3"," 23/5","12:00","1");
+
+
+INSERT INTO galletitas(Categoria,relleno,precio)
+VALUES
+("chocolate","sin relleno ","$1500");
+
+
+
+CREATE TABLE "pedidos" (
+	"pedidosID"	INTEGER NOT NULL,
+	"ClientesID"	INTEGER NOT NULL,
+	"fecha"	CURRENT_TIMESTAMP,
+	"hora"	INT,
+	"cantidad"	INT NOT NULL,
+	FOREIGN KEY("ClientesID") REFERENCES "Clientes"("ClientesID"),
+	PRIMARY KEY("pedidosID" AUTOINCREMENT)
+)
+
+
+
+
+INSERT INTO pedidos (ClientesID,fecha,hora,cantidad)
+VALUES (1,"2025-05-20","15:44:25","3");
